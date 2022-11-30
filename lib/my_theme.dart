2e1f02f60b619d005d11367cf2e58c56e300bba1
ibrahimzaher <90471809+ibrahimzaher@users.noreply.github.com>
2022-11-29 18:07:48 -0800

    added light and dark theme

---
+++ lib/my_theme.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static Color blackColor = Color(0xff242424);
+  static Color whiteColor = Color(0xffF8F8F8);
+  static Color primaryColor = Color(0xffB7935F);
+  static Color primaryDarkColor = Color(0xff141A2E);
+  static ThemeData light = ThemeData(
+    primaryColor: primaryColor,
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: blackColor,
+      ),
+    ),
+    primaryTextTheme: TextTheme(
+      headline1: TextStyle(
+        fontSize: 30,
+        color: blackColor,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: blackColor,
+      unselectedItemColor: whiteColor,
+      elevation: 0,
+    ),
+  );
+  static ThemeData dark = ThemeData(
+    primaryColor: primaryDarkColor,
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: whiteColor,
+      ),
+    ),
+    primaryTextTheme: TextTheme(
+      headline1: TextStyle(
+        fontSize: 30,
+        color: whiteColor,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: blackColor,
+      unselectedItemColor: whiteColor,
+      elevation: 0,
+    ),
+  );
+}
